25f5d5fda885e1e2317d569b8546c1a935d0304a
Richard Garbutt <richard@1000mileweb.com>
2015-07-08 15:40:23 +0200

    #47166 #47720 Finish releases implementation

--- lib/whatsnew/release.rb
+++ lib/whatsnew/release.rb
@@ -6,7 +6,7 @@ module Whatsnew
     @all_releases = nil
     attr_reader :version
 
-    def self.release_dir
+    def self.releases_dir
       ::Whatsnew.configuration.releases_dir
     end
 
@@ -16,13 +16,52 @@ module Whatsnew
     end
 
     def self.reload
-      release_file = YAML::load_file(File.join(self.release_dir, 'releases.yml'))
+      release_file = YAML::load_file(File.join(self.releases_dir, 'releases.yml'))
       @all_releases = release_file.map {|v| Release.new(v) }
     end 
 
+    # Returns the latest release (first in the yaml file)
+    def self.last
+      self.all.first
+    end
+
+    def self.new_release_since?( version )
+      self.last.version != version
+    end
+
     def initialize(version)
       @version = version
-      # @has_abstract = File.exists?(abstract_file)
+      @has_abstract = File.exists?(abstract_file)
     end
+
+    def markdown(variant = nil)
+      unless variant.nil?
+        variant_file = "#{Release.releases_dir}/release-#{@version}.#{variant}.md"
+        return IO.read(variant_file) if File.exists?(variant_file)
+      end
+
+      regular_file = "#{Release.releases_dir}/release-#{@version}.md"
+      if File.exists?(regular_file)
+        IO.read(regular_file)
+      else
+        nil
+      end
+    end
+
+    def abstract
+      IO.read(abstract_file) if abstract?
+    end
+
+    def abstract?
+      @has_abstract
+    end
+
+    private
+
+      def abstract_file
+        "#{Release.releases_dir}/release-abstract-#{@version}.md"
+      end
+
+
   end
 end
--- test/test_whatsnew.rb
+++ test/test_whatsnew.rb
@@ -1,96 +1,76 @@
 require 'minitest_helper'
 
-class TestWhatsnew < Minitest::Test
+module Whatsnew
+  class TestWhatsnew < Minitest::Test
 
-  def setup
-    Whatsnew.configure do |config|
-      config.releases_dir = File.expand_path( 'examples', __dir__ )
+    def setup
+      ::Whatsnew.configure do |config|
+        config.releases_dir = File.expand_path( 'examples', __dir__ )
+      end
     end
+
+    def test_that_it_has_a_version_number
+      refute_nil ::Whatsnew::VERSION
+    end
+
+    def test_release_all_returns_list_of_releases
+      releases = Release.all
+
+      assert_equal %W{5.0.1 5.0.0 4.3.2 4.2.1}, releases.map(&:version)
+    end
+
+    def test_last_returns_the_latest_version
+      assert_equal "5.0.1", Release.last.version
+    end
+
+    def test_new_release_since_is_true_if_new_release
+      assert Release.new_release_since?( "5.0.0" )
+    end
+
+    def test_new_release_since_is_false_if_no_new_release
+      assert !Release.new_release_since?( "5.0.1" )
+    end
+    
+    def test_release_markdown_returns_markdown
+      release = Release.new "5.0.1"
+      assert_equal "# Dude, check this awesome software!", release.markdown
+    end
+
+    def test_release_markdown_returns_variant_if_file_exists
+      release = Release.new "5.0.0"
+      assert_equal "# Starwood variant", release.markdown(:starwood)
+    end
+
+    def test_release_markdown_returns_regular_if_no_varient_exists
+      release = Release.new "5.0.1"
+      assert_equal "# Dude, check this awesome software!", release.markdown(:starwood)
+    end
+
+    def test_release_markdown_returns_nil_for_non_existent_releases
+      release = Release.new "6.6.6"
+
+      assert_nil release.markdown
+    end
+
+    def test_release_abstract_returns_contents_if_file_exists
+      release = Release.new "5.0.0"
+      assert_equal "Abstract of release 5.0.0", release.abstract
+    end
+
+    def test_release_abstract_returns_true_if_file_exists
+      release = Release.new "5.0.0"
+      assert_equal true, release.abstract?
+    end
+
+    def test_release_abstract_returns_nil_if_file_not_exists
+      release = Release.new "5.0.1"
+      assert_equal nil, release.abstract
+    end
+
+    def test_release_abstract_returns_false_if_file_not_exists
+      release = Release.new "5.0.1"
+      assert_equal false, release.abstract?
+    end
+
   end
-
-  def test_that_it_has_a_version_number
-    refute_nil ::Whatsnew::VERSION
-  end
-
-  def test_release_all_returns_list_of_releases
-    releases = ::Whatsnew::Release.all
-
-    assert_equal %W{5.0.1 5.0.0 4.3.2 4.2.1}, releases.map(&:version)
-  end
-
-  # test "Release.last returns the lastest version" do
-  #   assert_equal "5.0.1", Release.last.version
-  # end
-
-  # test "Release.new_release_for? returns true if the user hasn't viewed the latest release note" do
-  #   user = FactoryGirl.create :user, latest_version: "5.0.0"
-
-  #   assert Release.new_release_for?(user)
-  # end
-
-  # test "Release.new_release_for? returns false if the user has viewed the latest release note" do
-  #   user = FactoryGirl.create :user, latest_version: "5.0.1"
-
-  #   assert !Release.new_release_for?(user)
-  # end
-
-  # test "Release#markdown returns markdown notes for a release" do
-  #   release = Release.new "5.0.1"
-
-  #   assert_equal "# Dude, check this awesome software!", release.markdown
-  # end
-
-  # test "Release#markdown returns the installation variant if exists" do
-  #   release = Release.new "5.0.0"
-
-  #   assert_equal "# Starwood variant", release.markdown(:starwood)
-  # end
-
-  # test "Release#markdown returns a regular variant if no installation specific variant exists" do
-  #   release = Release.new "5.0.1"
-
-  #   assert_equal "# Dude, check this awesome software!", release.markdown(:starwood)
-  # end
-
-  # test "Release#markdown returns nil for non-existent releases" do
-  #   release = Release.new "6.6.6"
-
-  #   assert_nil release.markdown
-  # end
-
-  # test "Release#abstract returns file contents if it exists" do
-  #   release = Release.new "5.0.0"
-
-  #   assert_equal "Abstract of release 5.0.0", release.abstract
-  # end
-
-  # test "Release#abstract? returns true if it exists" do
-  #   release = Release.new "5.0.0"
-
-  #   assert_equal true, release.abstract?
-  # end
-
-  # test "Release#abstract returns nil if it doesn't exist" do
-  #   release = Release.new "5.0.1"
-
-  #   assert_equal nil, release.abstract
-  # end
-
-  # test "Release#abstract? returns false if it doesn't exist" do
-  #   release = Release.new "5.0.1"
-
-  #   assert_equal false, release.abstract?
-  # end
-
-  # test "Release#view sets the users latest_version attribute" do
-  #   release = Release.new "5.0.1"
-
-  #   user = FactoryGirl.create :user
-
-  #   release.view!(user)
-
-  #   user.reload
-
-  #   assert_equal "5.0.1", user.latest_version
-  # end
 end
